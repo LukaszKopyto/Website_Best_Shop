202786720ee8c9d1d37e47e73e69edb44d9b839a
Lukasz Kopyto <lukasz.kopyto@gmail.com>
2020-07-10 17:41:29 +0200

    adding hamburger menu operations

--- js/app.js
+++ js/app.js
@@ -1,142 +1,27 @@
-const menu = document.querySelector('.hamburger')
-const menuList = document.querySelector('.header__list')
-const select = document.querySelector('.custom-select')
-const customSelect = document.querySelector('.custom-select-wrapper')
-const calculateInput = document.querySelector('.calculate__input--box')
-const packageOption = calculateInput.querySelectorAll('.custom-option')
-const calculateResult = document.querySelector('.calculate__result')
-const sumOfAll = calculateResult.querySelector('.calculate__total--sum')
-const accounting = document.querySelector('.calculate__accounting')
-const paymentTerminal = document.querySelector('.calculate__terminal')
-let totalCost = 0
-let costOfProducts = 0
-let ordersInMonth = 0
-let selectedPackage = 0
-let accountingCost = 0
-let terminalCost = 0
+window.addEventListener('DOMContentLoaded', (event) => {
+  const menu = document.querySelector('.hamburger')
+  const menuList = document.querySelector('.header__list')
+  const menuItems = document.querySelectorAll('.header__items a')
 
-!!menu &&
-  menu.addEventListener('click', () => {
+  function toggleMenu() {
     menu.classList.toggle('hamburger--active')
     menuList.classList.toggle('hide')
-  })
-
-!!customSelect &&
-  customSelect.addEventListener('click', function () {
-    this.querySelector('.custom-select').classList.toggle('open')
-    this.querySelector('.arrow').classList.toggle('arrow-rotate')
-  })
-
-for (const option of document.querySelectorAll('.custom-option')) {
-  option.addEventListener('click', function () {
-    if (!this.classList.contains('selected')) {
-      this.parentNode
-        .querySelector('.custom-option.selected')
-        .classList.remove('selected')
-      this.classList.add('selected')
-      this.closest('.custom-select').querySelector(
-        '.custom-select__trigger span'
-      ).textContent = this.textContent
-    } else this.closest('.custom-select').querySelector('.custom-select__trigger span').textContent = this.textContent
-  })
-}
-
-window.addEventListener('click', function (e) {
-  if (!select.contains(e.target)) {
-    select.classList.remove('open')
   }
-})
 
-calculateInput.firstElementChild.addEventListener('change', function () {
-  calculateResult.querySelector('.calculate__product').classList.remove('hide')
-  calculateResult.querySelector(
-    '.calculate__product--option'
-  ).firstElementChild.textContent = this.value
-  costOfProducts = parseInt(this.value) * 0.5
-  calculateResult.querySelector(
-    '.calculate__product--price'
-  ).firstElementChild.textContent = costOfProducts
-  costSum()
-})
+  menu.addEventListener('click', () => {
+    toggleMenu()
+  })
+  for (let i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener('click', () => {
+      toggleMenu()
+    })
+  }
 
-calculateInput.children[1].addEventListener('change', function () {
-  calculateResult.querySelector('.calculate__orders').classList.remove('hide')
-  calculateResult.querySelector(
-    '.calculate__orders--option'
-  ).firstElementChild.textContent = this.value
-  ordersInMonth = parseInt(this.value) * 0.25
-  calculateResult.querySelector(
-    '.calculate__orders--price'
-  ).firstElementChild.textContent = ordersInMonth
-  costSum()
-})
-
-packageOption.forEach((element) => {
-  element.addEventListener('click', function () {
-    calculateResult
-      .querySelector('.calculate__package')
-      .classList.remove('hide')
-    calculateResult.querySelector(
-      '.calculate__package--option'
-    ).textContent = this.textContent
-    if (this.textContent === 'Basic') {
-      selectedPackage = 20
-    } else if (this.textContent === 'Professional') {
-      selectedPackage = 40
-    } else {
-      selectedPackage = 60
+  //close menu when clicking outside
+  window.addEventListener('mouseup', (e) => {
+    if (!menuList.contains(e.target) && !menu.contains(e.target)) {
+      menu.classList.remove('hamburger--active')
+      menuList.classList.add('hide')
     }
-    calculateResult.querySelector(
-      '.calculate__package--price'
-    ).firstElementChild.textContent = selectedPackage
-    costSum()
   })
 })
-
-function accountingChecked() {
-  accounting.classList.remove('hide')
-  accountingCost = 20
-}
-
-function accountingUnchecked() {
-  accounting.classList.add('hide')
-  accountingCost = 0
-}
-
-calculateInput.children[3].addEventListener('change', function (e) {
-  this.firstElementChild.checked ? accountingChecked() : accountingUnchecked()
-  calculateResult.querySelector(
-    '.calculate__accounting--price'
-  ).firstElementChild.textContent = accountingCost
-  costSum()
-})
-
-function paymentTerminalChecked() {
-  paymentTerminal.classList.remove('hide')
-  terminalCost = 5
-}
-
-function paymentTerminalUnchecked() {
-  paymentTerminal.classList.add('hide')
-  terminalCost = 0
-}
-
-calculateInput.children[4].addEventListener('change', function () {
-  this.firstElementChild.checked
-    ? paymentTerminalChecked()
-    : paymentTerminalUnchecked()
-  calculateResult.querySelector(
-    '.calculate__terminal--price'
-  ).firstElementChild.textContent = terminalCost
-  costSum()
-})
-
-function costSum() {
-  totalCost =
-    costOfProducts +
-    ordersInMonth +
-    selectedPackage +
-    accountingCost +
-    terminalCost
-  return (sumOfAll.textContent = totalCost)
-}
